322e9018276fa1abb9fa26deed09956c44801730
Francesco Doronzo <francescodrnz@gmail.com>
2022-12-02 18:13:45 +0100

    Day 1: puzzle 2 solved

--- src/main/java/com/aoc2/services/CalorieCounting.java
+++ src/main/java/com/aoc2/services/CalorieCounting.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.time.Instant;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -14,6 +16,9 @@ public class CalorieCounting {
     static final String ELF = "elf";
 
     public static void main(String[] args) {
+
+        Instant start = Instant.now();
+
         File file = new File("./externalfile/calories.txt");
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -31,8 +36,14 @@ public class CalorieCounting {
             }
             int maxCalories = elvesMap.values().stream().mapToInt(i -> i).max().getAsInt();
             log.info("maximum calories: " + maxCalories);
+
+            int top3CaloriesSum = elvesMap.values().stream().sorted(Comparator.comparingInt(Integer::intValue).reversed()).limit(3).mapToInt(i -> i).sum();
+            log.info("sum of top three calories: " + top3CaloriesSum);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            log.info("elapsed time (ms): "+ (Instant.now().toEpochMilli() - start.toEpochMilli()));
         }
     }
 }
